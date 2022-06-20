2b531f2515f444a685a3641e91391b2d92c9bda3
ivan <59779051+ivangithead@users.noreply.github.com>
2022-06-20 12:57:04 +0300

    Update my_chat_member.py

--- handlers/content_types/my_chat_member.py
+++ handlers/content_types/my_chat_member.py
@@ -1,6 +1,12 @@
+import logging
 from aiogram import types
 from main import dp, bot
 
+logger = logging.getLogger("bot.handlers.content_types.my_chat_member")
+
 @dp.my_chat_member_handler()
 async def my_chat_member(message: types.Message) -> None:
-	await bot.leave_chat(message.chat.id)
+	try:
+		await bot.leave_chat(message.chat.id)
+	except Exception as ex:
+		logger.critical(ex)
