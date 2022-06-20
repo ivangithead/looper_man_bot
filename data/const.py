e803f576e9d4eb25a8edaece732b2c5812681308
ivan <59779051+ivangithead@users.noreply.github.com>
2022-06-20 11:30:43 +0300

    Update const.py

--- data/const.py
+++ data/const.py
@@ -2,5 +2,5 @@ SUB_TRIAL_TIME = 172800 # 2 дня
 DB_PATH = "data/database.sqlite3"
 BOT_AUTHOR = "@headofcoders"
 ADMINS = [493979238]
-RATE_LIMIT = 2
+RATE_LIMIT = 5
 LOGGER_FORMAT = "%(asctime)s - %(name)s:%(lineno)s - %(levelname)s - %(message)s"
